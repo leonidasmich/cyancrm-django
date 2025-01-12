8da99b6f972a7f4d9ae3e4379ebf94a0cc0cf40b
Leonidas Michalopoulos <mich.lewn@gmail.com>
2025-01-13 01:53:09 +0200

    first commit

---
+++ cyancrm/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth import views
+from django.urls import path, include
+
+from core.views import index
+from userprofile.views import signup
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('dashboard/leads/', include('lead.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('sign-up/', signup, name='signup'),
+    path('login-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-out/', views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+]
